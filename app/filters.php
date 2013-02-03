a6a051e55c5aff72a83e196edf5d7720b462f9bf
Bruno G. <brunofgaspar1@gmail.com>
2013-02-03 20:42:20 +0000

    random needed files

---
+++ app/filters.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application & Route Filters
+|--------------------------------------------------------------------------
+|
+| Below you will find the "before" and "after" events for the application
+| which may be used to do any work before or after a request into your
+| application. Here you may also register your custom route filters.
+|
+*/
+
+App::before(function($request)
+{
+	//
+});
+
+
+App::after(function($request, $response)
+{
+	//
+});
+
+/*
+|--------------------------------------------------------------------------
+| Authentication filter.
+|--------------------------------------------------------------------------
+|
+| This filter is usefull to verify if the user of the current session is
+| logged into the application.
+|
+*/
+
+Route::filter('auth', function()
+{
+	// Check if the user is logged in
+	if ( ! Sentry::check())
+	{
+		// Store the current uri in the session
+		Session::put('loginRedirect', Request::url());
+
+		// Redirect to the login page
+		return Redirect::to('account/login');
+	}
+});
+
+/*
+|--------------------------------------------------------------------------
+| Admin authentication filter.
+|--------------------------------------------------------------------------
+|
+| This filter does the same as the 'auth' filter but it checks if the user
+| has 'admin' privileges.
+|
+*/
+
+Route::filter('admin-auth', function()
+{
+	// Check if the user is logged in
+	if ( ! Sentry::check())
+	{
+		// Store the current uri in the session
+		Session::put('loginRedirect', Request::url());
+
+		// Redirect to the login page
+		return Redirect::to('account/login');
+	}
+
+	// Check if the user has access to the admin page
+	if ( ! Sentry::getUser()->hasAccess('admin'))
+	{
+		// Show the insufficient permissions page
+		return View::make('error/403');
+	}
+});
+
+/*
+|--------------------------------------------------------------------------
+| CSRF Protection Filter
+|--------------------------------------------------------------------------
+|
+| The CSRF filter is responsible for protecting your application against
+| cross-site request forgery attacks. If this special token in a user
+| session does not match the one given in this request, we'll bail.
+|
+*/
+
+Route::filter('csrf', function()
+{
+	if (Session::getToken() != Input::get('csrf_token'))
+	{
+		throw new Illuminate\Session\TokenMismatchException;
+	}
+});
---
+++ app/routes.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Register all the admin routes.
+|
+*/
+
+# Blog Management
+Route::get('admin/blogs', 'AdminBlogsController@getIndex');
+Route::get('admin/blogs/create', 'AdminBlogsController@getCreate');
+Route::post('admin/blogs/create', 'AdminBlogsController@postCreate');
+Route::get('admin/blogs/{blogId}/edit', 'AdminBlogsController@getEdit');
+Route::post('admin/blogs/{blogId}/edit', 'AdminBlogsController@postEdit');
+Route::get('admin/blogs/{blogId}/delete', 'AdminBlogsController@getDelete');
+
+# User Management
+Route::get('admin/users', 'AdminUsersController@getIndex');
+Route::get('admin/users/create', 'AdminUsersController@getCreate');
+Route::post('admin/users/create', 'AdminUsersController@postCreate');
+Route::get('admin/users/{userId}/edit', 'AdminUsersController@getEdit');
+Route::post('admin/users/{userId}/edit', 'AdminUsersController@postEdit');
+Route::get('admin/users/{userId}/delete', 'AdminUsersController@getDelete');
+
+# Group Management
+Route::get('admin/groups', 'AdminGroupsController@getIndex');
+Route::get('admin/groups/create', 'AdminGroupsController@getCreate');
+Route::post('admin/groups/create', 'AdminGroupsController@postCreate');
+Route::get('admin/groups/{groupId}/edit', 'AdminGroupsController@getEdit');
+Route::post('admin/groups/{groupId}/edit', 'AdminGroupsController@postEdit');
+Route::get('admin/groups/{groupId}/delete', 'AdminGroupsController@getDelete');
+
+# Admin Dashboard
+Route::get('admin', 'AdminDashboardController@getIndex');
+
+
+/*
+|--------------------------------------------------------------------------
+| Authentication and Authorization Routes
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+
+# Login
+Route::get('account/login', 'AuthController@getLogin');
+Route::post('account/login', 'AuthController@postLogin');
+
+# Register
+Route::get('account/register', 'AuthController@getRegister');
+Route::post('account/register', 'AuthController@postRegister');
+
+# Account Activation
+Route::get('account/activate/{userID}/{activationCode}', 'AuthController@getActivate');
+
+# Forgot Password
+Route::get('account/forgot-password', 'AuthController@getForgotPassword');
+Route::post('account/forgot-password', 'AuthController@postForgotPassword');
+
+# Forgot Password Confirmation
+Route::get('account/forgot-password/{userID}/{resetCode}', 'AuthController@getForgotPasswordConfirmation');
+Route::post('account/forgot-password/{userID}/{resetCode}', 'AuthController@postForgotPasswordConfirmation');
+
+# Logout
+Route::get('account/logout', 'AuthController@getLogout');
+
+
+/*
+|--------------------------------------------------------------------------
+| Account Routes
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+
+# Settings
+Route::get('account/settings', 'AccountSettingsController@getIndex');
+Route::post('account/settings', 'AccountSettingsController@postIndex');
+
+# Dashboard
+Route::get('account', 'AccountDashboardController@getIndex');
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('{postSlug}', 'BlogController@getView');
+Route::post('{postSlug}', 'BlogController@postView');
+
+Route::get('/', 'BlogController@getIndex');
