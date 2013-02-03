e4eba8656c5e8995e7da25b2788fc34a74fb93a4
Bruno G. <brunofgaspar1@gmail.com>
2013-02-03 20:43:15 +0000

    admin dashboard

---
+++ app/controllers/admin/AdminDashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+class AdminDashboardController extends AdminController {
+
+	/**
+	 * Show the administration dashboard page.
+	 *
+	 * @return View
+	 */
+	public function getIndex()
+	{
+		// Show the page
+		return View::make('admin/dashboard');
+	}
+
+}
