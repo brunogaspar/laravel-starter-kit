0c50f6e53a91d4a0d902a2fb51c7024daf0a203d
Bruno G. <brunofgaspar1@gmail.com>
2013-02-09 21:59:37 +0000

    resetting config data

--- app/config/database.php
+++ app/config/database.php
@@ -54,8 +54,8 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
-			'database'  => 'laravel4_testing',
+			'host'      => 'localhost',
+			'database'  => 'database',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
