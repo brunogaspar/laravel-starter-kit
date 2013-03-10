c0e59f99f52e694fb65cc62bebb88ce656fa5518
OpenCubicles <opencubicles@gmail.com>
2013-03-10 18:07:47 +0530

    Update welcome.blade.php
    fixed to use activationcode passed from the controller instead of generating activation code twice in view.

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -7,7 +7,7 @@
 
 		<p>Welcome to SiteNameHere! Please click on the following link to confirm your SiteNameHere account:</p>
 
-		<p><a href="{{ URL::to('account/activate/' . $user->id . '/' . $user->getActivationCode()) }}">{{ URL::to('account/activate/' . $user->id . '/' . $user->getActivationCode()) }}</a></p>
+		<p><a href="{{ URL::to('account/activate/' . $user->id . '/' . $activationcode }}">{{ URL::to('account/activate/' . $user->id . '/' . $activationcode }}</a></p>
 
 		<p>Best regards,</p>
 
