df8eca3fbce972f05df4a26fb4f6a18e213dc0fb
Bruno G. <brunofgaspar1@gmail.com>
2013-03-26 04:06:43 +0000

    update the error views

--- app/views/error/403.blade.php
+++ app/views/error/403.blade.php
@@ -100,9 +100,7 @@
 	<div class="wrapper">
 		<div class="error-spacer"></div>
 		<div role="main" class="main">
-			<?php $messages = array('We need a map.', 'I think we\'re lost.', 'We took a wrong turn.'); ?>
-
-			<h1>Unn</h1>
+			<h1>Access Forbidden</h1>
 
 			<h2>Server Error: 403 (Forbidden)</h2>
 
@@ -111,13 +109,11 @@
 			<h3>What does this mean?</h3>
 
 			<p>
-				We couldn't find the page you requested on our servers. We're really sorry
-				about that. It's our fault, not yours. We'll work hard to get this page
-				back online as soon as possible.
+				You don't have the necessary permissions to access to this page.
 			</p>
 
 			<p>
-				Perhaps you would like to go to our <a href="{{ URL::to('/'); }}">home page</a>?
+				Perhaps you would like to go to our <a href="{{ URL::route('home'); }}">home page</a>?
 			</p>
 		</div>
 	</div>
