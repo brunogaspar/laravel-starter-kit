979128d44aea29b16cd9d6330539832661136ed3
Bruno G. <brunofgaspar1@gmail.com>
2013-05-31 04:33:23 +0100

    tweak the bootstrap presenter

--- app/presenters/paginator/BootstrapPresenter.php
+++ app/presenters/paginator/BootstrapPresenter.php
@@ -1,64 +1,8 @@
 <?php namespace Presenters\Paginator;
 
-use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\BootstrapPresenter as Presenter;
 
-class BootstrapPresenter {
-
-	/**
-	 * The paginator instance being rendered.
-	 *
-	 * @var \Illuminate\Pagination\Paginator
-	 */
-	protected $paginator;
-
-	/**
-	 * The current page of the paginator.
-	 *
-	 * @var int
-	 */
-	protected $currentPage;
-
-	/**
-	 * The last available page of the paginator.
-	 *
-	 * @var int
-	 */
-	protected $lastPage;
-
-	/**
-	 * Create a new Bootstrap presenter instance.
-	 *
-	 * @param  \Illuminate\Pagination\Paginator  $paginator
-	 * @return void
-	 */
-	public function __construct(Paginator $paginator)
-	{
-		$this->paginator   = $paginator;
-		$this->lastPage    = $this->paginator->getLastPage();
-		$this->currentPage = $this->paginator->getCurrentPage();
-	}
-
-	/**
-	 * Render the Bootstrap pagination contents.
-	 *
-	 * @return string
-	 */
-	public function render()
-	{
-		// The hard-coded thirteen represents the minimum number of pages we need to
-		// be able to create a sliding page window. If we have less than that, we
-		// will just render a simple range of page links insteadof the sliding.
-		if ($this->lastPage < 13)
-		{
-			$content = $this->getPageRange(1, $this->lastPage);
-		}
-		else
-		{
-			$content = $this->getPageSlider();
-		}
-
-		return $this->getPrevious().$content.$this->getNext();
-	}
+class BootstrapPresenter extends Presenter {
 
 	/**
 	 * Create a range of pagination links.
@@ -87,82 +31,6 @@ class BootstrapPresenter {
 		}
 
 		return implode('', $pages);
-	}
-
-	/**
-	 * Create a pagination slider link window.
-	 *
-	 * @return string
-	 */
-	protected function getPageSlider()
-	{
-		$window = 6;
-
-		// If the current page is very close to the beginning of the page range, we will
-		// just render the beginning of the page range, followed by the last 2 of the
-		// links in this list, since we will not have room to create a full slider.
-		if ($this->currentPage <= $window)
-		{
-			$ending = $this->getFinish();
-
-			return $this->getPageRange(1, $window + 2).$ending;
-		}
-
-		// If the current page is close to the ending of the page range we will just get
-		// this first couple pages, followed by a larger window of these ending pages
-		// since we're too close to the end of the list to create a full on slider.
-		elseif ($this->currentPage >= $this->lastPage - $window)
-		{
-			$start = $this->lastPage - 8;
-
-			$content = $this->getPageRange($start, $this->lastPage);
-
-			return $this->getStart().$content;
-		}
-
-		// If we have enough room on both sides of the current page to build a slider we
-		// will surround it with both the beginning and ending caps, with this window
-		// of pages in the middle providing a Google style sliding paginator setup.
-		else
-		{
-			$content = $this->getAdjacentRange();
-
-			return $this->getStart().$content.$this->getFinish();
-		}
-	}
-
-	/**
-	 * Get the page range for the current page window.
-	 *
-	 * @return string
-	 */
-	public function getAdjacentRange()
-	{
-		$start = $this->currentPage - 3;
-
-		return $this->getPageRange($start, $this->currentPage + 3);
-	}
-
-	/**
-	 * Create the beginning leader of a pagination slider.
-	 *
-	 * @return string
-	 */
-	public function getStart()
-	{
-		return $this->getPageRange(1, 2).$this->getDots();
-	}
-
-	/**
-	 * Create the ending cap of a pagination slider.
-	 *
-	 * @return string
-	 */
-	public function getFinish()
-	{
-		$content = $this->getPageRange($this->lastPage - 1, $this->lastPage);
-
-		return $this->getDots().$content;
 	}
 
 	/**
@@ -232,28 +100,6 @@ class BootstrapPresenter {
 		$url = $this->paginator->getUrl($page);
 
 		return '<li><a href="'.$url.'">'.$page.'</a></li>';
-	}
-
-	/**
-	 * Set the value of the current page.
-	 *
-	 * @param  int   $page
-	 * @return void
-	 */
-	public function setCurrentPage($page)
-	{
-		$this->currentPage = $page;
-	}
-
-	/**
-	 * Set the value of the last page.
-	 *
-	 * @param  int   $page
-	 * @return void
-	 */
-	public function setLastPage($page)
-	{
-		$this->lastPage = $page;
 	}
 
 }
